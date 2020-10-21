782f7c30a3e231e5d5ef1d63d1f2f634bd56fb70
Martyna Markiel <martyna.markiel@gmail.com>
2020-10-21 22:02:04 +0200

    airbnb copy without show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+flat1 = Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+flat2 = Flat.create!(
+  name: 'Green & Spacious Garden Flat Barcelona',
+  address: '10 Clifton Gardens Barcelona W9 1DT',
+  description: 'An amazing summer feel for this spacious flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 55,
+  number_of_guests: 3
+)
+
+flat3 = Flat.create!(
+  name: 'Blue & Sad Flat Madrid',
+  address: '15 Clifton Madrid W9 1DT',
+  description: 'An amazing summer feel for this spacious flat. Three double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 60,
+  number_of_guests: 3
+)
+
+flat4 = Flat.create!(
+  name: 'Yellow & Sunny Garden Flat Barcelona',
+  address: '11 Clifton Gardens Barcelona 123',
+  description: 'Sunny feel for this spacious flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 70,
+  number_of_guests: 5
+)
